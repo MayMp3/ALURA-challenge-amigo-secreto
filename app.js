6250572539f024fe1d988d3a85ba3b6f8b6aaa39
Mayara Silva <masantos181203@gmail.com>
2025-01-30 22:57:23 -0300

    Adicionado função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []; //armazenará os nomes inseridos
+
+function adicionarAmigo(amigo) { //função pra adicionar amigos digitados
+    amigo = document.querySelector ('input').value; 
+    //pega o valor digitado e adiciona no parametro da funç.
+
+    if (amigo != '') { //se o campo estiver diferente de vazio
+        amigos.push (amigo); //junta no array dos amigos lá em cima
+    } else { //se não, pede pra inserir um valor válido
+        alert('Por favor, insira um nome.');
+    }
+    //limparCampo ();
+    //chamada da função que limpa a caixa de texto
+  
+    alert(amigos); //teste pra ver se tá juntando no array
+}
+
+/*function limparCampo () {
+    amigo = document.querySelector ('input');
+    amigo.value = '';
+    //limpar caixa de texto
+}
+
