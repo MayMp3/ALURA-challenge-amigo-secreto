d046fab03ee244755196997808e2cbe9b4626a1e
Mayara Silva <masantos181203@gmail.com>
2025-02-01 12:21:29 -0300

    adicionado função que exibe nomes na tela (com bug de duplicata)

--- app.js
+++ app.js
@@ -4,16 +4,28 @@ function adicionarAmigo(amigo) { //função pra adicionar amigos digitados
     amigo = document.querySelector ('input').value; 
     //pega o valor digitado e adiciona no parametro da funç.
 
-    if (amigo != '') { //se o campo estiver diferente de vazio
-        amigos.push (amigo); //junta no array dos amigos lá em cima
+    if (amigo !== '') { //se o campo estiver diferente de vazio
+        amigos.push (amigo);  //junta no array dos amigos lá em cima
+
     } else { //se não, pede pra inserir um valor válido
         alert('Por favor, insira um nome.');
     }
+    atualizarAmigos();
     limparCampo ();
     //chamada da função que limpa a caixa de texto
-  
-    alert(amigos); //teste pra ver se tá juntando no array
+    //alert(amigos); //teste pra ver se tá juntando no array
 }
+
+function atualizarAmigos () {
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+        //obrigada pela ajuda ! Linn do Discord!
+    }
+    
+}
+listaAmigos.innerHTML = '';
 
 function limparCampo () {
     amigo = document.querySelector ('input');
